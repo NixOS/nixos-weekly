f7304d9a6e7f2082953d9a278af80c415fb5af57
Eric Sagnes <eric.sagnes@gmail.com>
2016-09-28 21:21:55 +0900

    default.nix: refactoring
    - separated the templates in a dedicated nix file
    - added a configuration file

---
+++ conf.nix
@@ -0,0 +1,17 @@
+{
+  # Directory where posts are located
+  postsDir = ./posts;
+
+  # Directory where static files are located
+  staticDir = ./static;
+
+  # URL of the website
+  siteUrl = "http://weekly.nixos.org";
+
+  # maximum number of posts on the index page
+  postsOnIndexPage = 10;
+
+  # maximum number of posts per archive page
+  # null to have all older posts on first archive page
+  postsPerArchivePage = 100;
+}
