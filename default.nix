63969d9aa77c23085ae04ae16b6e673cd95b9336
Eric Sagnes <eric.sagnes@gmail.com>
2016-09-28 21:55:44 +0900

    default.nix: separate lib

--- default.nix
+++ default.nix
@@ -9,8 +9,10 @@
 , current_timestamp  # date -u +%Y-%m-%dT%TZ
 }:
 
-with pkgs.lib;
 with builtins;
+
+let lib = import ./lib.nix { inherit pkgs; };
+  in with lib;
 
 let
 
@@ -19,77 +21,9 @@ let
 
   state = { inherit current_timestamp; };
 
-  templates = import ./templates.nix { inherit conf state; lib = pkgs.lib // { inherit prettyTimestamp; };  };
+  templates = import ./templates.nix { inherit conf state lib; };
 
-  markdown = pkgs.callPackage ({ stdenv, fetchzip }: stdenv.mkDerivation {
-    name = "markdown-1.0.1";
-    src = fetchzip {
-      url = http://daringfireball.net/projects/downloads/Markdown_1.0.1.zip;
-      sha256 = "1mic1v7cliz59h04pj1gw001wzh346aw3dvb266agj706bg79kdf";
-    };
-    phases = [ "installPhase" ];
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src/Markdown.pl $out/bin/markdown
-      sed -i '1s:/usr/bin/perl:${pkgs.perl}/bin/perl:' $out/bin/markdown
-    '';
-  }) {};
-
-  prettyTimestamp = timestamp:
-    let
-      year = substring 0 4 timestamp;
-      day = substring 8 2 timestamp;
-      month' = substring 5 2 timestamp;
-      month =
-             if month' == "01" then "JAN"
-        else if month' == "02" then "FEB"
-        else if month' == "03" then "MAR"
-        else if month' == "04" then "APR"
-        else if month' == "05" then "MAY"
-        else if month' == "06" then "JUN"
-        else if month' == "07" then "JUL"
-        else if month' == "08" then "AUG"
-        else if month' == "09" then "SEP"
-        else if month' == "10" then "OCT"
-        else if month' == "11" then "NOV"
-        else if month' == "12" then "DEC"
-        else abort "Unknown month (${month'}) for date (${timestamp}).";
-    in
-      "${day} ${month} ${year}";
-
-  parsePost = filename:
-    let
-      result = match "(....-..-..)-(.*)\.md" filename;
-      timestamp = elemAt result 0;
-      id = elemAt result 1;
-      path = "${conf.postsDir}/${filename}";
-      year = substring 0 4 timestamp;
-      href = "posts/${timestamp}-${id}.html";
-      html = pkgs.runCommand "${timestamp}.html" {} ''
-        ${markdown}/bin/markdown < ${path} > $out
-      '';
-      title = readFile (pkgs.runCommand "${timestamp}-${id}.title" {} ''
-        ${pkgs.xidel}/bin/xidel ${html} -e "//h1[1]/node()" -q > $out
-        echo -n `tr -d '\n' < $out` > $out
-      '');
-    in
-      if result == null then
-        abort "Post (${filename}) not in correct form (YYYY-MM-DD-<id>.md)."
-      else if title == "" then
-        abort "Post (${filename}) does not include title (h1)."
-      else
-        {
-          inherit timestamp href title id;
-          html = readFile html;
-        };
-
-  posts =
-    sort
-      (a: b: lessThan b.timestamp a.timestamp)
-      (filter (x: x != null)
-             (map parsePost
-                  (attrNames (filterAttrs (_: v: v == "regular")
-                             (readDir conf.postsDir)))));
+  posts = getPosts conf.postsDir;
 
   pages =
     let
@@ -118,15 +52,6 @@ let
             else "archive${toString (i - 1)}.html";
         template = templates.archive;
       }) (tail chunks);
-
-  chunksOf = k:
-    let
-      f = ys: xs:
-        if xs == []
-          then ys
-          else f (ys ++ [(take k xs)]) (drop k xs);
-    in
-      f [];
 
 
 in pkgs.runCommand "nixos-weekly" {} ''
