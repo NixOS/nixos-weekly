962bc75bfa387e4e96ea17334bff54c201dfbadc
Domen Kožar <domen@dev.si>
2021-01-21 12:52:32 +0100

    get lint to work again

--- lint.sh
+++ lint.sh
@@ -1,4 +1,6 @@
 BIN=$(nix-build nixpkgs.nix -A nodePackages_10_x.markdown-link-check)/bin/markdown-link-check
 
-find content/2019 -type f -exec $BIN {} \;
+for file in content/$(date +%Y)/*; do
+  $BIN $file
+done
 
